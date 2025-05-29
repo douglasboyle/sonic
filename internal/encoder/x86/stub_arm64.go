fce4cba37b2798dd2532f28d4473cc46dd97ce07
douglasboyle <douglas.boyle@samsara.com>
2025-05-29 16:15:11 -0700

    fix: create stub file for internal/encoder/x86
    Problem: CI was failing because the internal/encoder/x86 package had build
    constraints that excluded ALL files on non-AMD64 architectures, making Go think
    the package didn't exist.
    
    Root Cause: When we added //go:build amd64 constraints to all files in the x86
    package, we created a situation where on ARM64 systems (like your CI
    environment), every single file was excluded, leaving an empty package.
    
    Solution: Created a stub file internal/encoder/x86/stub_arm64.go with //go:build
    !amd64 that ensures the package exists on non-AMD64 architectures, even though
    it contains no functionality.

---
+++ internal/encoder/x86/stub_arm64.go
@@ -0,0 +1,24 @@
+//go:build !amd64
+// +build !amd64
+
+/*
+ * Copyright 2021 ByteDance Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package x86
+
+// This file exists to ensure the x86 package compiles on non-AMD64 architectures
+// even though all implementation files are excluded by build constraints.
+// The actual x86-specific functionality is only available on AMD64.
